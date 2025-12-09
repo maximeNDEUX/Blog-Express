152e9ee9feffd1e218bd3d5f262a11bac08afe47
maxime_n2 <maxime.mercier@hotmail.com>
2025-12-09 11:07:55 +0100

    layout css structure

---
+++ public/asset/script.js
@@ -0,0 +1,11 @@
+// alert("Ceci est une alerte");
+
+// const name = prompt("Entrez votre nom :");
+// console.log(`Bonjour, ${name} !`);
+
+// const isOk = confirm("Est-ce que tout va bien ?");
+// if (isOk) {
+//   console.log("Tout va bien !");
+// } else {
+//   console.log("Il y a un problème.");
+// }
